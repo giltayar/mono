d603d30bafd7b0405b5b01d8cac57dd2def2734b
Gil Tayar <gil@tayar.org>
2020-11-18 16:18:44 +0200

    create: remove all obsolete package templates

--- packages/create/src/package-templates/app-ts/test/unit/templatetemplate.unit.test.ts
+++
@@ -1,9 +0,0 @@
-import {describe, it} from 'mocha'
-import {expect} from 'chai'
-import createApp from '../../src/templatetemplate'
-
-describe('templatetemplate (unit)', function() {
-  it('should be able to load (you can delete this test once you have others)', async () => {
-    expect(createApp).to.not.be.undefined
-  })
-})
--- packages/create/src/package-templates/library-ts/src/templatetemplate.ts
+++
@@ -1,9 +0,0 @@
-export interface Add {
-  a: number
-  b: number
-}
-export function add({a, b}: Add) {
-  const ret = a + b
-
-  return ret
-}
