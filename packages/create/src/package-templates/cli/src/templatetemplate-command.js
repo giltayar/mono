8fecb68e10a14e9cf8365c74a68d9f81d479c370
Gil Tayar <gil@tayar.org>
2020-11-20 11:31:04 +0200

    create: remove non-working templates

--- packages/create/src/package-templates/cli/src/templatetemplate-command.js
+++
@@ -1,5 +0,0 @@
-async function main({_optionOption}) {
-  //
-}
-
-module.exports = main
--- packages/create/src/package-templates/cli/test/e2e/templatetemplate.e2e.test.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const {describe, it} = require('mocha')
-const {expect} = require('chai')
-
-describe('templatetemplate e2e', function () {
-  it('should return OK on /', async () => {
-    expect(4).to.equal(4)
-  })
-})
