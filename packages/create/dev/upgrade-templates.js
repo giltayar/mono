94cfbd04cb32d6bbfba024bd21ec0189cf013c85
Gil Tayar <gil@tayar.org>
2020-11-21 09:15:09 +0200

    create: update "library" template
    [bilt-with-bilt]

--- packages/create/dev/upgrade-templates.js
+++ packages/create/dev/upgrade-templates.js
@@ -10,16 +10,29 @@ for (const dir of await fs.promises.readdir(packageTemplatesDir, {
   withFileTypes: true,
 })) {
   if (!dir.isDirectory) continue
+  const packageDir = path.join(packageTemplatesDir, dir.name)
 
-  console.log(`upgrading template "${dir.name}"`)
+  console.log(`upgrading template "${dir.name}"`, packageDir)
 
-  const upgraded = await ncu.run({cwd: path.join(packageTemplatesDir, dir.name), upgrade: true})
+  await fs.promises.rename(
+    path.join(packageDir, '.package.json'),
+    path.join(packageDir, 'package.json'),
+  )
+  try {
+    process.chdir(packageDir)
+    const upgraded = await ncu.run({upgrade: true})
 
-  for (const [packageName, version] of Object.entries(upgraded)) {
-    console.log(packageName, '==>', version)
-  }
+    for (const [packageName, version] of Object.entries(upgraded)) {
+      console.log(packageName, '==>', version)
+    }
 
-  if (Object.entries(upgraded).length === 0) {
-    console.log('nothing to upgrade')
+    if (Object.entries(upgraded).length === 0) {
+      console.log('nothing to upgrade')
+    }
+  } finally {
+    await fs.promises.rename(
+      path.join(packageDir, 'package.json'),
+      path.join(packageDir, '.package.json'),
+    )
   }
 }
