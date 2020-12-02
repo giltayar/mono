6f4d46d0f8bcd8afbc5e9533c404fafef30dbe12
Gil Tayar <gil@tayar.org>
2020-12-02 21:17:35 +0200

    create: support base packages
    [bilt-with-bilt]

--- packages/create/src/create-package.js
+++ packages/create/src/create-package.js
@@ -8,6 +8,69 @@ const {ncp} = ncpPackage
 const __filename = new URL(import.meta.url).pathname
 const __dirname = path.dirname(__filename)
 
+if (process.argv.length < 3) {
+  console.error('usage: create <template-name>')
+  process.exit(1)
+}
+
+/**
+ * @param {string} templateName
+ * @param {{onlyCopyFiles?: boolean}} [options]
+ */
+async function createFromTemplate(templateName, {onlyCopyFiles = false} = {}) {
+  const templateDir = path.resolve(__dirname, 'package-templates', templateName)
+  const targetDir = process.cwd()
+
+  if (!fs.existsSync(templateDir)) {
+    console.error('could not find template', templateName)
+    process.exit(1)
+  }
+
+  if (fs.existsSync(path.join(templateDir, '.base'))) {
+    await createFromTemplate(
+      (await fs.promises.readFile(path.join(templateDir, '.base'), 'utf-8')).trim(),
+      {onlyCopyFiles: true},
+    )
+  }
+
+  console.log(`Copying files from: ${templateDir} to: ${targetDir}`)
+
+  await p(ncp)(templateDir, targetDir)
+
+  if (onlyCopyFiles) {
+    return
+  }
+
+  const targetDirArr = targetDir.split('/')
+  const packageName = targetDirArr[targetDirArr.length - 1]
+
+  await fs.promises.rename(`${targetDir}/.package.json`, `${targetDir}/package.json`)
+  if (fs.existsSync(`${targetDir}/..gitignore`)) {
+    await fs.promises.rename(`${targetDir}/..gitignore`, `${targetDir}/.gitignore`)
+  }
+
+  const allFiles = walkSync(targetDir)
+
+  for (const file of allFiles) {
+    const newFileName = file.replaceAll('templatetemplate', packageName)
+    console.log('creating file', newFileName)
+    const content = (await fs.promises.readFile(file, 'utf8')).replaceAll(
+      'templatetemplate',
+      packageName,
+    )
+
+    await fs.promises.writeFile(file, content, 'utf8')
+
+    if (newFileName != file) {
+      await fs.promises.rename(file, newFileName)
+    }
+  }
+}
+
+await createFromTemplate(process.argv[2])
+
+console.log('Done!')
+
 /**
  *
  * @param {string} dir
@@ -23,60 +86,3 @@ function walkSync(dir, filelist = []) {
   })
   return filelist
 }
-
-/**
- *
- * @param {string} string
- * @param {string} search
- * @param {string} replacement
- *
- * @returns {string}
- */
-function replaceAll(string, search, replacement) {
-  return string.replace(new RegExp(search, 'g'), replacement)
-}
-
-if (process.argv.length < 3) {
-  console.error('usage: create <template-name>')
-  process.exit(1)
-}
-
-const templateName = process.argv[2]
-
-const templateDir = path.resolve(__dirname, 'package-templates', templateName)
-if (!fs.existsSync(templateDir)) {
-  console.error('could not find template', templateName)
-  process.exit(1)
-}
-const targetDir = process.cwd()
-const targetDirArr = targetDir.split('/')
-const packageName = targetDirArr[targetDirArr.length - 1]
-
-console.log(`Copying files from: ${templateDir} to: ${targetDir}`)
-
-await p(ncp)(templateDir, process.cwd())
-
-await fs.promises.rename(`${targetDir}/.package.json`, `${targetDir}/package.json`)
-if (fs.existsSync(`${targetDir}/..gitignore`)) {
-  await fs.promises.rename(`${targetDir}/..gitignore`, `${targetDir}/.gitignore`)
-}
-
-const allFiles = walkSync(targetDir)
-
-for (const file of allFiles) {
-  const newFileName = replaceAll(file, 'templatetemplate', packageName)
-  console.log('creating file', newFileName)
-  const content = replaceAll(
-    await fs.promises.readFile(file, 'utf8'),
-    'templatetemplate',
-    packageName,
-  )
-
-  await fs.promises.writeFile(file, content, 'utf8')
-
-  if (newFileName != file) {
-    await fs.promises.rename(file, newFileName)
-  }
-}
-
-console.log('Done!')
