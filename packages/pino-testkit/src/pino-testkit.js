ca5ad3dd891bad139d993fcaf6483c702429b367
Gil Tayar <gil@tayar.org>
2020-12-09 10:00:01 +0200

    pt: made log recorder global
    [bilt-with-bilt]

--- packages/pino-testkit/src/pino-testkit.js
+++ packages/pino-testkit/src/pino-testkit.js
@@ -1,29 +1,28 @@
-/**
- * This is for testing purposes only
- * @returns {{
- *  record: () => void,
- *  playback: () => import('type-fest').JsonObject[],
- *  loggerOptionsForRecorder: import('pino').LoggerOptions
- * }}
- */
-export function makeRecorder() {
-  /**@type {any[]} */
-  let logs = []
-
-  return {
-    record() {
-      logs = []
-    },
-    playback() {
-      return logs
-    },
-    loggerOptionsForRecorder: {
-      prettifier: () => /** @param {import('type-fest').JsonObject} log */ (log) => {
-        logs.push(log)
-
-        return JSON.stringify(log) + '\n'
-      },
-      prettyPrint: true,
-    },
-  }
+export function recordLogs() {
+  //@ts-expect-error
+  globalThis[pinoTestkitGlobalSymbol] = []
 }
+
+/**
+ * @returns {import('type-fest').JsonObject}
+ */
+export function playbackLogs() {
+  //@ts-expect-error
+  return globalThis[pinoTestkitGlobalSymbol]
+}
+
+export const loggerOptionsForRecorder = {
+  prettifier: () => /** @param {import('type-fest').JsonObject} log */ (log) => {
+    // @ts-expect-error
+    globalThis[pinoTestkitGlobalSymbol].push(log)
+
+    return JSON.stringify(log) + '\n'
+  },
+  prettyPrint: true,
+}
+
+const pinoTestkitGlobalSymbol = Symbol('pino-testkit-globals')
+
+/**@type {import('type-fest').JsonObject[]} */
+//@ts-expect-error
+globalThis[pinoTestkitGlobalSymbol] = []
--- packages/pino-testkit/test/unit/pino-testkit.unit.test.js
+++ packages/pino-testkit/test/unit/pino-testkit.unit.test.js
@@ -2,39 +2,37 @@ import chai from 'chai'
 import mocha from 'mocha'
 import pino from 'pino'
 import chaiSubset from 'chai-subset'
-import {makeRecorder} from '../../src/pino-testkit.js'
+import {loggerOptionsForRecorder, recordLogs, playbackLogs} from '../../src/pino-testkit.js'
 const {describe, it} = mocha
 const {expect, use} = chai
 use(chaiSubset)
 
 describe('pino-testkit (unit)', function () {
   it('you dont have to call record the first time', async () => {
-    const {loggerOptionsForRecorder, playback} = makeRecorder()
-
     const logger = pino(loggerOptionsForRecorder)
+    recordLogs()
 
     logger.info({a: 4}, 'hello')
     logger.info('world')
 
-    expect(playback()).to.have.length(2)
-    expect(playback()).to.containSubset([{a: 4, msg: 'hello'}, {msg: 'world'}])
+    expect(playbackLogs()).to.have.length(2)
+    expect(playbackLogs()).to.containSubset([{a: 4, msg: 'hello'}, {msg: 'world'}])
   })
 
   it('record erases the playback', async () => {
-    const {loggerOptionsForRecorder, record, playback} = makeRecorder()
-
     const logger = pino(loggerOptionsForRecorder)
+    recordLogs()
 
     logger.info({a: 4}, 'hello')
     logger.info('world')
 
-    expect(playback()).to.have.length(2)
-    expect(playback()).to.containSubset([{a: 4, msg: 'hello'}, {msg: 'world'}])
+    expect(playbackLogs()).to.have.length(2)
+    expect(playbackLogs()).to.containSubset([{a: 4, msg: 'hello'}, {msg: 'world'}])
 
-    record()
+    recordLogs()
 
     logger.info({a: 5}, 'a message')
-    expect(playback()).to.have.length(1)
-    expect(playback()).to.containSubset([{a: 5, msg: 'a message'}])
+    expect(playbackLogs()).to.have.length(1)
+    expect(playbackLogs()).to.containSubset([{a: 5, msg: 'a message'}])
   })
 })
