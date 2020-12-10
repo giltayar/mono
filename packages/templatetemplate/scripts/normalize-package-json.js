97b4bf67ecd7ff61b9becdf13ac05640d4a8d225
Gil Tayar <gil@tayar.org>
2020-12-10 09:46:20 +0200

    templatetemplate: remove it after copying it to create templates

--- packages/templatetemplate/scripts/normalize-package-json.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// eslint-disable-next-line node/shebang
-import fs from 'fs'
-
-const packageJson = JSON.parse(await fs.promises.readFile('package.json'))
-
-packageJson.version = '0.0.1'
-
-await fs.promises.writeFile('.temp/package.json', JSON.stringify(packageJson, null, 2))
