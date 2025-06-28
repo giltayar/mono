4d09480c75685f9c39466186e54123888220b66d
Gil Tayar <giltayar@microsoft.com>
2025-06-28 11:19:38 +0300

    sc: fixed eslint configuration

--- packages/service-commons/src/bind.ts
+++ packages/service-commons/src/bind.ts
@@ -1,15 +1,15 @@
 export type ServiceFunction<ServiceData, TArgs extends unknown[], TReturn> = (
-  _service: ServiceData,
-  ..._args: TArgs
+  service: ServiceData,
+  ...args: TArgs
 ) => TReturn
 
 export function bind<ServiceData, TArgs extends unknown[], TReturn>(
   fn: ServiceFunction<ServiceData, TArgs, TReturn>,
   service: ServiceData,
 ) {
-  return fn.bind(service, service) as (..._args: TArgs) => TReturn
+  return fn.bind(service, service) as (...args: TArgs) => TReturn
 }
 
 export type ServiceBind<ServiceData> = <TArgs extends unknown[], TReturn>(
-  _f: ServiceFunction<ServiceData, TArgs, TReturn>,
-) => (..._args: TArgs) => TReturn
+  f: ServiceFunction<ServiceData, TArgs, TReturn>,
+) => (...args: TArgs) => TReturn
