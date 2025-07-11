79dc64118c753c6aaa79ecb65679bc624f76234f
Gil Tayar <giltayar@microsoft.com>
2025-07-11 16:29:17 +0300

    cc: consolidate all club stuff into one cli

---
+++ packages/carmel-tools/club-cli/src/club-cli.ts
@@ -0,0 +1,59 @@
+import {createClubServiceFromClub} from './create-club-service.ts'
+import * as clubs from './clubs/clubs.ts'
+import yargs from 'yargs'
+import type {Clubs} from './clubs/club-types.ts'
+
+await yargs()
+  .option('club', {
+    type: 'string',
+    choices: Object.keys(clubs) as Clubs[],
+    demandOption: true,
+  })
+  .command(
+    'expire-subscriptions',
+    'Expire subscriptions for a club',
+    () => {},
+    async (args) => {
+      const clubService = createClubServiceFromClub(clubs[args.club])
+
+      await clubService.paymentExpiration()
+    },
+  )
+  .command(
+    'handle-cancel-requests',
+    'Handle cancel requests for a club',
+    () => {},
+    async (args) => {
+      const clubService = createClubServiceFromClub(clubs[args.club])
+
+      await clubService.dealWithCancelRequests()
+    },
+  )
+  .command(
+    'handle-debt',
+    'Handle users with debt for a club',
+    () => {},
+    async (args) => {
+      const clubService = createClubServiceFromClub(clubs[args.club])
+
+      await clubService.dealWithUsersWithDebt()
+    },
+  )
+  .command(
+    'remove-user <user-email>',
+    'Remove a user from a club',
+    (yargs) =>
+      yargs.positional('user-email', {
+        type: 'string',
+        demandOption: true,
+        describe: 'Email of the user to remove',
+      }),
+    async (args) => {
+      const clubService = createClubServiceFromClub(clubs[args.club])
+
+      await clubService.removeUser(args['user-email'])
+    },
+  )
+  .strict()
+  .help()
+  .parseAsync(process.argv.slice(2))
