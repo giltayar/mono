77249b8fb37c6211361db9095b5ab0e7a987e916
Gil Tayar <giltayar@microsoft.com>
2025-10-07 13:23:41 +0300

    wip

---
+++ packages/carmbo/carmbo-app/src/sql/04-sale.sql
@@ -0,0 +1,68 @@
+DO $$ BEGIN
+    CREATE TYPE SALE_OPERATION AS ENUM ('create');
+EXCEPTION
+    WHEN duplicate_object THEN null;
+END $$;
+
+CREATE TABLE
+  IF NOT EXISTS sale (
+    sale_number SERIAL PRIMARY KEY,
+    last_history_id UUID,
+    last_data_id UUID -- denormalized for performance
+  );
+
+CREATE TABLE
+  IF NOT EXISTS sale_history (
+    id UUID PRIMARY KEY,
+    data_id UUID NOT NULL,
+    sale_number SERIAL,
+    timestamp TIMESTAMPTZ,
+    operation HISTORY_OPERATION NOT NULL,
+    operation_reason TEXT
+  );
+
+CREATE UNIQUE INDEX IF NOT EXISTS sale_history_idx ON sale_history (sale_number, timestamp, operation);
+
+CREATE TABLE
+  IF NOT EXISTS sale_search (
+    data_id UUID NOT NULL,
+    searchable_text TEXT NOT NULL
+  );
+
+CREATE UNIQUE INDEX IF NOT EXISTS sale_search_idx ON sale_search (data_id);
+
+CREATE TABLE
+  IF NOT EXISTS sale_data (
+    data_id UUID NOT NULL,
+    name TEXT NOT NULL,
+    sale_event_number INTEGER NOT NULL,
+    student_number INTEGER NOT NULL,
+    final_sale_revenue NUMERIC(10,2)
+  );
+
+CREATE UNIQUE INDEX IF NOT EXISTS sale_data_idx ON sale_data (data_id);
+
+CREATE TABLE IF NOT EXISTS sale_product (
+  data_id UUID NOT NULL,
+  item_order INTEGER NOT NULL,
+  product_number INTEGER NOT NULL,
+  quantity INTEGER NOT NULL, -- 0 means it was not bought
+  unit_price NUMERIC(10,2) -- not necessarily what it was bought for. Just what was the price asked for
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS sale_product_idx ON sale_product (data_id, item_order);
+
+
+CREATE TABLE
+  IF NOT EXISTS sale_data_cardcom (
+    data_id UUID NOT NULL,
+    response_json JSONB NOT NULL,
+    invoice_number TEXT NOT NULL,
+    terminal_number: TEXT NOT NULL,
+    approval_number: TEXT NOT NULL,
+    sale_timestamp TIMESTAMPTZ NOT NULL,
+    user_email: TEXT,
+    coupon: TEXT,
+  );
+
+CREATE UNIQUE INDEX IF NOT EXISTS sale_data_cardcom_idx ON sale_data_cardcom (data_id);
